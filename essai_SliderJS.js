7d34ace888fe2504e1b086d45f4140866f9640b8
Rachel Volat <rachel1arvl@gmail.com>
2022-04-14 08:39:36 +0200

    fichier temp _ slider defilement next; previous en cours

--- essai_SliderJS.js
+++ essai_SliderJS.js
@@ -1,79 +1,80 @@
 /* Liste des affiches FILMS LES MIEUX NOTES (source provisoire,
 à remplacer par requête(s) API)
 */
-const thumbnailsFilms = document.querySelectorAll('.slider img');
-let thumbnailsList = [];
-for (let thumbnail in thumbnailsFilms){
-    thumbnailsList.push(thumbnail);
-}
-console.log(thumbnailsList); //pr vérifier liste --> comment afficher résultat ds console ?
+const availableThumbnails = ["pictures/image1.jpg", "pictures/image2.jpg", 
+                                "pictures/image3.jpg", "pictures/image4.jpg", 
+                                "pictures/image5.jpg", "pictures/image6.jpg", 
+                                "pictures/image7.jpg"];
+console.log(availableThumbnails);
 
-
-//nombre d'affiches
-const thumbnailsNumber = thumbnailsList.length;
-console.log(thumbnailsNumber); //pr vérif
-//boutons 'suivant' 'précédent'
-const next = document.querySelector('.nav-droite > img');
-const previous = document.querySelector('.nav-gauche > img');
+let activeThumbnails = document.getElementsByClassName('active');
+console.log(activeThumbnails);
 
 let count = 0;
 
-//retire 4 vignettes d'affilée
-function removeFilmsThumbnails(thumbnailsList, count){
-    thumbnailsList[count].classList.remove('active');
-    thumbnailsList[count++].classList.remove('active');
-    thumbnailsList[count += 2].classList.remove('active');
-    thumbnailsList[count += 3].classList.remove('active');
+//boutons 'suivant' 'précédent'
+const nextThumbnail = document.querySelector('.nav-droite > img'); // ok
+const previousThumbnail = document.querySelector('.nav-gauche > img'); // ok
+
+//Change les 4 images du carrousel  (fonction ok)
+function changePic(availableThumbnails, activeThumbnails, count){
+    for (let pictIndex = 0; pictIndex < 4; pictIndex++){
+        let pict1 = activeThumbnails[pictIndex];
+        let pict1Src = pict1.getAttribute("src");
+        let newPict = availableThumbnails[count + pictIndex];
+        pict1Src = newPict;
+        pict1.setAttribute("src", pict1Src);
+    }
 }
 
-//affiche 4 vignettes d'affilée
-function displayFilmsThumbnails(count){
-    thumbnailsList[count].classList.add('active');
-    thumbnailsList[count++].classList.add('active');
-    thumbnailsList[count += 2].classList.add('active');
-    thumbnailsList[count += 3].classList.add('active');
+//Enchaine la dernière image avec la 1ère et les suivantes
+function changeEndOfListPic(availableThumbnails, activeThumbnails, count){
+    for (let pictIndex = 0; pictIndex < 4; pictIndex++){
+        let pict1 = activeThumbnails[pictIndex];
+        let pict1Src = pict1.getAttribute("src");
+        
+        if ((count + pictIndex) < availableThumbnails.length){
+            let newPict = availableThumbnails[availableThumbnails.length - (availableThumbnails.length - count - pictIndex)];
+            console.log(newPict);
+            pict1Src = newPict;
+            pict1.setAttribute("src", pict1Src);
+        } else {
+            let newPict = availableThumbnails[count + pictIndex - (availableThumbnails.length)];
+            console.log(newPict);
+            pict1Src = newPict;
+            pict1.setAttribute("src", pict1Src);
+    
+        }
+    }
 }
 
 
-//AFFICHE FILMS SUIVANTS : ma version 4 images
-function nextSlides(count){
-    //masque les vignettes affichées et incrémente le compteur
-    removeFilmsThumbnails(count);
-    count += 3;
 
-    if(count < thumbnailsNumber - 1){   //pour 12 slides : count 11 = 12-1 cf ELSE
-        count++;
-        displayFilmsThumbnails(count);
+// affiche images suivantes quand utilisateur clique sur bouton 'suivant'
+nextThumbnail.addEventListener('click', function(){
+    count++;
+    if(count < availableThumbnails.length - 3){
+        changePic(availableThumbnails, activeThumbnails, count);
     } else {
-        count = 0;
-        displayFilmsThumbnails(count);  //count reste à 0
+        if (count > availableThumbnails.length - 1){
+            count = 0; //
+            changePic(availableThumbnails, activeThumbnails, count);
+        } else {
+            changeEndOfListPic(availableThumbnails, activeThumbnails, count)
+        }    
     }
-}
-
-//appelle la fonction AFFICHE FILMS SUIVANTS sur un clic de souris
-next.addEventListener('click', nextSlides);
+    console.log(count);
+});
 
 
-//AFFICHE FILMS PRECEDENTS : ma version 4 images
-function previousSlides(count){
-    //masque les vignettes affichées
-    removeFilmsThumbnails(count);
-
-    if(count > 0){  //count  = 0 cf ELSE 
-        count -= 4;  //affiche les 4 images précédentes 
-        displayFilmsThumbnails(count);
-    } else { //qd count = 0,  affiche les derniers slides de la liste 
-        count = thumbnailsNumber - 4; //pr 12 images, count = 12-4 = 8
-        displayFilmsThumbnails(count);
-    }
-}
-
-//appelle la fonction AFFICHE FILMS PRECEDENTS sur un clic de souris
-previous.addEventListener('click', previousSlides);
-
-
-/*
-QUE SE PASSE T IL SI  nb de films non multiple de 4 - ex. 10 films
-qd .add('active') puis .remove('active'), renvoie quelle valeur/quel message 
-pour films 9 et 10 ? 'null' ?
-*/
+// affiche images précédentes quand utilisateur clique sur bouton 'précédent'
+previousThumbnail.addEventListener('click', function(){
+    count--;
+    if(count < 0){
+        count = 3;
+        changePic(availableThumbnails, activeThumbnails, count);
+    } else {
+        changePic(availableThumbnails, activeThumbnails, count);
+        }
+    console.log(count);
+});
