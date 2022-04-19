bf3358e9507c56634edc65afbcfd1ea2e3bd9006
Rachel Volat <rachel1arvl@gmail.com>
2022-04-19 09:31:30 +0200

    sliders version POO

--- essai_SliderJS.js
+++ essai_SliderJS.js
@@ -1,68 +1,183 @@
-/* Liste des affiches FILMS LES MIEUX NOTES (source provisoire,
-à remplacer par requête(s) API)
-*/
-const availableThumbnails = ["pictures/image1.jpg", "pictures/image2.jpg", 
-                                "pictures/image3.jpg", "pictures/image4.jpg", 
-                                "pictures/image5.jpg", "pictures/image6.jpg", 
-                                "pictures/image7.jpg"];
-console.log(availableThumbnails);
+//availableThumbnails : 1er et 2e slider
 
-let activeThumbnails = document.getElementsByClassName('active');
-console.log(activeThumbnails);
+let imagesBestFilms = ["pictures/image1.jpg", "pictures/image2.jpg", 
+                        "pictures/image3.jpg", "pictures/image4.jpg", 
+                        "pictures/image5.jpg", "pictures/image6.jpg", 
+                        "pictures/image7.jpg"];
 
-let count = 0;
-console.log('compteur =' + count);
+let imagesAventure = ["pictures/imageA.jpg", "pictures/imageB.jpg", 
+                        "pictures/imageC.jpg", "pictures/imageD.jpg", 
+                        "pictures/imageE.jpg", "pictures/imageF.jpg", 
+                        "pictures/imageG.jpg"];
 
-//boutons 'suivant' 'précédent'
-const nextThumbnail = document.querySelector('.nav-droite > img'); // ok
-const previousThumbnail = document.querySelector('.nav-gauche > img'); // ok
+let imagesAnimation = ["pictures/image1.jpg", "pictures/image2.jpg", 
+                        "pictures/image3.jpg", "pictures/image4.jpg", 
+                        "pictures/image5.jpg", "pictures/image6.jpg", 
+                        "pictures/image7.jpg"];
 
-/*Change les 4 images du carrousel.
-pictIndex = index des 4 vignettes 'actives'
-*/
-function changeThumbnail(availableThumbnails, activeThumbnails, count){
-    for (let pictIndex = 0; pictIndex < 4; pictIndex++){
-        let pict1 = activeThumbnails[pictIndex];
-        let pict1Src = pict1.getAttribute("src");
-        if ((count + pictIndex) < 0){
-            let newPict = availableThumbnails[availableThumbnails.length + (count + pictIndex)];
-            console.log(newPict);
-            pict1Src = newPict;
-            pict1.setAttribute("src", pict1Src);
-        } else {
-            let newPict = availableThumbnails[count + pictIndex];
-            console.log(newPict);
-            pict1Src = newPict;
-            pict1.setAttribute("src", pict1Src);
+let imagesBiographie = ["pictures/imageA.jpg", "pictures/imageB.jpg", 
+                        "pictures/imageC.jpg", "pictures/imageD.jpg", 
+                        "pictures/imageE.jpg", "pictures/imageF.jpg", 
+                        "pictures/imageG.jpg"];
+
+
+//activeThumbnails des 4 sliders
+
+let activePicSlider1 = document.querySelectorAll('.category1 .slider .active');
+let activePicSlider2 = document.querySelectorAll('.category2 .slider .active');
+let activePicSlider3 = document.querySelectorAll('.category3 .slider .active');
+let activePicSlider4 = document.querySelectorAll('.category4 .slider .active');
+
+// boutons 'suivant' 'précédent'des 4 sliders
+let nextThumbnail1 = document.querySelector('.category1 .nav-droite > img');
+let previousThumbnail1 = document.querySelector('.category1 .nav-gauche > img');
+
+let nextThumbnail2 = document.querySelector('.category2 .nav-droite > img');
+let previousThumbnail2 = document.querySelector('.category2 .nav-gauche > img');
+
+let nextThumbnail3 = document.querySelector('.category3 .nav-droite > img');
+let previousThumbnail3 = document.querySelector('.category3 .nav-gauche > img');
+
+let nextThumbnail4 = document.querySelector('.category4 .nav-droite > img');
+let previousThumbnail4 = document.querySelector('.category4 .nav-gauche > img');
+
+let count1 = 0;
+let count2 = 0;
+let count3 = 0;
+let count4 = 0;
+
+console.log('compteur initial1 =' + count1);
+
+
+class Slider{
+    constructor(activeThumbnails, availableThumbnails, nextThumbnail, previousThumbnail, count){
+        this.activeThumbnails = activeThumbnails;
+        this.availableThumbnails = availableThumbnails;
+        this.nextThumbnail = nextThumbnail;
+        this.previousThumbnail = previousThumbnail;
+        this.count = count;
+    }
+    
+    incrementPreviousThumbnail(){
+        this.count--;
+        console.log('count1 -- = ' + this.count);
+        if(this.count > 3){
+            this.count = - 3;
+        } else if (this.count < - 7){
+            this.count = -1;
+            }
+        return this.count;
+    }
+
+    incrementNextThumbnail(){
+        this.count++;
+        console.log('count1 ++ = ' + this.count);
+        if(this.count > 3){ 
+            this.count = - 3;
+        } else if (this.count < - 7){
+            this.count = -1;
+            }
+        return this.count;
+    }
+
+    changeThumbnail(){
+        for (let pictIndex = 0; pictIndex < 4; pictIndex++){
+            let pict1 = this.activeThumbnails[pictIndex];
+            let pict1Src = pict1.getAttribute("src");
+            console.log('count dans changeThumbnail = ' + this.count);
+            if ((this.count + pictIndex) < 0){
+                let newPict = this.availableThumbnails[this.availableThumbnails.length + (this.count + pictIndex)];
+                console.log(newPict);
+                pict1Src = newPict;
+                pict1.setAttribute("src", pict1Src);
+            } else {
+                console.log(this.count);
+                console.log(pictIndex);
+                let newPict = this.availableThumbnails[this.count + pictIndex];
+                console.log(newPict);
+                pict1Src = newPict;
+                pict1.setAttribute("src", pict1Src);
+            }
         }
     }
 }
 
-/* affiche images précédentes quand utilisateur clique sur bouton 'précédent'.
-count = 3 affiche les 4 dernières images de la liste,
-count = -7 affiche la position de départ (4 premières images).
-*/
-previousThumbnail.addEventListener('click', function(){
-    count--;
-    console.log('count -- = ' + count);
-    if(count > 3){
-        count = - 3;
-    } else if (count < - 7){
-        count = -1;
-        }
-    console.log('count = ' + count);
-    changeThumbnail(availableThumbnails, activeThumbnails, count);
+
+// création des 4 sliders (4 instances de la classe Slider)
+let bestFilmsSlider = new Slider(activePicSlider1, 
+                        imagesBestFilms, 
+                        nextThumbnail1,
+                        previousThumbnail1,
+                        count1);
+
+let AventureSlider = new Slider(activePicSlider2, 
+                        imagesAventure, 
+                        nextThumbnail2,
+                        previousThumbnail2,
+                        count2);
+
+let AnimationSlider = new Slider(activePicSlider3, 
+                        imagesAnimation, 
+                        nextThumbnail3,
+                        previousThumbnail3,
+                        count3);
+
+let BiographieSlider = new Slider(activePicSlider4, 
+                        imagesBiographie, 
+                        nextThumbnail4,
+                        previousThumbnail4,
+                        count4);
+
+
+// boutons précédent/suivant 1
+
+bestFilmsSlider.previousThumbnail.addEventListener('click', function(){
+    bestFilmsSlider.incrementPreviousThumbnail(this.count);
+    console.log('count1 = ' + this.count);
+    bestFilmsSlider.changeThumbnail(this.activeThumbnails, this.availableThumbnails, this.count);
 });
 
-// affiche images suivantes quand utilisateur clique sur bouton 'suivant'.
-nextThumbnail.addEventListener('click', function(){
-    count++;
-    console.log('count ++ = ' + count);
-    if(count > 3){ 
-        count = - 3;
-    } else if (count < - 7){
-        count = -1;
-        }
-    console.log('count = ' + count);
-    changeThumbnail(availableThumbnails, activeThumbnails, count);
+bestFilmsSlider.nextThumbnail.addEventListener('click', function(){
+    bestFilmsSlider.incrementNextThumbnail(this.count);
+    console.log('count1 = ' + this.count);
+    bestFilmsSlider.changeThumbnail(this.activeThumbnails, this.availableThumbnails, this.count);
+});
+
+// boutons précédent/suivant 2
+AventureSlider.previousThumbnail.addEventListener('click', function(){
+    AventureSlider.incrementPreviousThumbnail(this.count);
+    console.log('count2 = ' + this.count);
+    AventureSlider.changeThumbnail(this.activeThumbnails, this.availableThumbnails, this.count);
+});
+
+AventureSlider.nextThumbnail.addEventListener('click', function(){
+    AventureSlider.incrementNextThumbnail(this.count);
+    console.log('count2 = ' + this.count);
+    AventureSlider.changeThumbnail(this.activeThumbnails, this.availableThumbnails, this.count);
+});
+
+// boutons précédent/suivant 3
+AnimationSlider.previousThumbnail.addEventListener('click', function(){
+    AnimationSlider.incrementPreviousThumbnail(this.count);
+    console.log('count2 = ' + this.count);
+    AnimationSlider.changeThumbnail(this.activeThumbnails, this.availableThumbnails, this.count);
+});
+
+AnimationSlider.nextThumbnail.addEventListener('click', function(){
+    AnimationSlider.incrementNextThumbnail(this.count);
+    console.log('count2 = ' + this.count);
+    AnimationSlider.changeThumbnail(this.activeThumbnails, this.availableThumbnails, this.count);
+});
+
+// boutons précédent/suivant 4
+BiographieSlider.previousThumbnail.addEventListener('click', function(){
+    BiographieSlider.incrementPreviousThumbnail(this.count);
+    console.log('count2 = ' + this.count);
+    BiographieSlider.changeThumbnail(this.activeThumbnails, this.availableThumbnails, this.count);
+});
+
+BiographieSlider.nextThumbnail.addEventListener('click', function(){
+    BiographieSlider.incrementNextThumbnail(this.count);
+    console.log('count2 = ' + this.count);
+    BiographieSlider.changeThumbnail(this.activeThumbnails, this.availableThumbnails, this.count);
 });
