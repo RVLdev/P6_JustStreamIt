06634ebac6ea2ad2c8af989ef5fdcc38058327cc
Rachel Volat <rachel1arvl@gmail.com>
2022-05-03 19:54:13 +0200

    carrousel Best rated Films = fonctionnel

--- essaiAJAX.js
+++ essaiAJAX.js
@@ -1,42 +1,3 @@
-// variables : utilité à vérifier
-
-let bestFilmTitle;
-let bestFilmImageUrl;
-let bestFilmDescription;
-let bestFilmDate;
-let bestFilmDuration;
-let bestFilmLongDescription;
-let bestFilmImbd;
-let bestFilmBoxOffice;
-let bestFilmActors;
-let bestFilmDirectors;
-let bestFilmGenres;
-let bestFilmCountries;
-let bestFilmRated;
-
-
-// Exemple pour tests : Meilleur Film = 'film1' ------------A SUPPRIMER-------------------
-let film1 = {
-    "title": "The Fantasticks", 
-    "date_published": "2000-09-22", 
-    "duration": 86, 
-    "description": "A mysterious fair that comes to a small community in the countryside could make real the illusions of two teenagers.", 
-    "long_description": "Two teenagers on neighboring farms steal glances and hide their romance from their feuding fathers. However, the fathers are actually good friends who have hatched a plan--with help from a mystical roving sideshow and its equally-mysterious ringmaster--to get the two lovebirds down the aisle. But in order to bring these families together, they must first be torn apart.", 
-    "avg_vote": "5.6", 
-    "imdb_score": "5.6", 
-    "worldwide_gross_income": 49666, 
-    "image_url": "https://m.media-amazon.com/images/M/MV5BMjIyMDE0NzY1M15BMl5BanBnXkFtZTgwOTgzMzgwMzE@._V1_UX182_CR0,0,182,268_AL_.jpg", 
-    "actors": ["Arturo Gil", "Barnard Hughes", "Brad Sullivan", "Dyrk Ashton",
-                "Gregory Amato", "Jean Louisa Kelly", "Joel Grey", 
-                "Joey McIntyre", "Jonathon Morris", "Lee Bell", 
-                "Shaunery Stevens", "Teller", "Tony Cox", "Trayne Thomas", 
-                "Victoria Stevens"], 
-    "directors": ["Michael Ritchie"], 
-    "genres": ["Musical", "Romance"], 
-    "countries": ["USA"], 
-    "rated": "PG"
-};
-
 // ----- 1 API - MEILLEUR FILM (classement imdb) -----
 let bestFilmsList = [];
 let bestFilmUrl;
@@ -44,7 +5,6 @@ let detailedbestFilmsList = [];
 
 // 1 Affiche le bloc "Meilleur Film"
 getBestFilmUrl();
-
 
 function getBestFilmUrl(){
     let httpRequest = new XMLHttpRequest();
@@ -72,7 +32,7 @@ function getBestFilmDetails(bestFilmUrl){
             if (httpRequest.status === 200) {
                 let detailedFilm = JSON.parse(httpRequest.responseText);
                 //console.log(detailedFilm);
-                getBestFilmTitle(detailedFilm)
+                getBestFilmTitle(detailedFilm);
             }
         }
     };
@@ -156,45 +116,73 @@ const buttonFilmDetails = document.getElementsByClassName("bouton_details");
 const modalElement = document.getElementById("modal1");
 const closingCross = document.getElementsByClassName("close");
 
-
-// Affiche la fenêtre modale qd clique sur bouton "Détails du film" -- A POURSUIVRE v27 04
+// 1 Affiche la fenêtre modale qd clique sur bouton "Détails du film"
 buttonFilmDetails[0].addEventListener('click', function(){
     modalElement.style.display = "flex";
 });
 
-// Ferme la fenêtre modale qd clique sur la croix
+// 1 Ferme la fenêtre modale qd clique sur la croix
 closingCross[0].addEventListener('click', function(){
     modalElement.style.display = "none";
 });
 
 
-// ----- 2 API - FILMS lES MIEUX NOTES (classement "rated") -----
-let bestRatedfilmsList = [];
-let bestRatedFilmsImages = [];
-let activePicSlider1 = document.querySelectorAll('.category1 .slider .active'); 
 
-// 2 Récupération des images pour carrousel "Films les mieux notés"
-function getBestRatedFilmsImages(){
+// ---------- 2 API - FILMS lES MIEUX NOTES (classement "rated") ----------
+var bestRatedFilmsList = [];
+var bestRatedFilmsImages = [];
+var activePicSlider1 = document.querySelectorAll('.category1 .slider .active');
+
+
+// 2 Récupération de la LISTE DES FILMS pour carrousel "Films les mieux notés"
+function getbestRatedFilmsList(){
     let httpRequest = new XMLHttpRequest();
     httpRequest.open('GET', "http://localhost:8000/api/v1/titles/?format=json&sort_by=rated,-year", true);
     httpRequest.send();
     httpRequest.onreadystatechange = function(){
         if(httpRequest.readyState === 4){
             if (httpRequest.status === 200) {
-                bestRatedfilmsList = JSON.parse(httpRequest.responseText);
-                console.log(bestRatedfilmsList);
-                getBestRatedFilmsImages(bestRatedfilmsList);
-                getAvailableFilms(bestRatedFilmsImages);
-                getBestRatedFilmDetails(bestRatedfilmsList);
-                return bestRatedfilmsList;
+                let filmsRequest = JSON.parse(httpRequest.responseText);
+                bestRatedFilmsList = filmsRequest.results;
+                console.log(bestRatedFilmsList);  // OK 5 éléments (films)
+                get2ndPageBestRatedFilms(bestRatedFilmsList);
             }
         }
     };
 }
 
-function getBestRatedFilmsImages(bestRatedfilmsList){
-    for(let ratedFilm = 0; ratedFilm < 5; ratedFilm++){
-        bestRatedFilmsImages.push(bestRatedfilmsList.results[ratedFilm].image_url);
+function get2ndPageBestRatedFilms(bestRatedFilmsList) {
+    console.log(bestRatedFilmsList);  // OK 5 éléments (films)
+    let httpRequest = new XMLHttpRequest();
+    httpRequest.open('GET', "http://localhost:8000/api/v1/titles/?format=json&page=2&sort_by=rated,-year", true);
+    httpRequest.send();
+    httpRequest.onreadystatechange = function(){
+        if(httpRequest.readyState === 4){
+            if (httpRequest.status === 200) {
+                let filmsRequestPage2 = JSON.parse(httpRequest.responseText);
+                let page2bestRatedFilmsList = filmsRequestPage2.results;
+                
+                bestRatedFilmsList.push(page2bestRatedFilmsList[0]);
+                bestRatedFilmsList.push(page2bestRatedFilmsList[1]);
+
+                console.log(bestRatedFilmsList);
+                console.log(bestRatedFilmsList.length);
+
+                getBestRatedFilmsImages(bestRatedFilmsList); //Liste URL images puis charge les 4x1ères ds carrousel
+                getDetailedRatedFilmsUrls(bestRatedFilmsList);
+                return bestRatedFilmsList;  // liste détaillée des films (best rated)
+            }    
+            
+        }
+    };
+}
+
+
+// 2 Récupération des images pour carrousel "Films les mieux notés"
+
+function getBestRatedFilmsImages(bestRatedFilmsList){
+    for(let ratedFilm in bestRatedFilmsList){
+        bestRatedFilmsImages.push(bestRatedFilmsList[ratedFilm].image_url);
     }
     console.log(bestRatedFilmsImages);
     loadRatedFilmsSliderImages(bestRatedFilmsImages);
@@ -213,7 +201,6 @@ function loadRatedFilmsSliderImages(bestRatedFilmsImages){
 }
 
 // *** 2 Création fenêtre MODALE 1ère image Carrousel FILMS LES MIEUX NOTES ***
-
 function createThumbnailModal(){
     //setThumbnailModalElements();
     setThumbnailModalElementsAttributes();
@@ -222,26 +209,26 @@ function createThumbnailModal(){
 
 // 2.1 Création  éléments de la MODALE (1ère) image Carrousel FILMS LES MIEUX NOTES
 //function setThumbnailModalElements(){}
-let newModal = document.createElement('aside');
-let newModalContent = document.createElement('div');
-let newModalHeader = document.createElement('div');
-let modalClose = document.createElement('div');
-let modalH2= document.createElement('h2');
-let newModalBody = document.createElement('div');
-let modalUl = document.createElement('ul');
-let modalNoList = document.createElement('li');
-let modalImage = document.createElement('img');
-let modalTitle = document.createElement('li');
-let modalPublishedDate = document.createElement('li'); 
-let modalDuration = document.createElement('li');
-let modalLong_Desc = document.createElement('li');
-let modalImdbScore = document.createElement('li');
-let modalBoxOffice = document.createElement('li');
-let modalActors = document.createElement('li');
-let modalDirectors = document.createElement('li');
-let modalGenres = document.createElement('li');
-let modalCountries = document.createElement('li');
-let modalRated = document.createElement('li');
+var newModal = document.createElement('aside');
+var newModalContent = document.createElement('div');
+var newModalHeader = document.createElement('div');
+var modalClose = document.createElement('div');
+var modalH2= document.createElement('h2');
+var newModalBody = document.createElement('div');
+var modalUl = document.createElement('ul');
+var modalNoList = document.createElement('li');
+var modalImage = document.createElement('img');
+var modalTitle = document.createElement('li');
+var modalPublishedDate = document.createElement('li'); 
+var modalDuration = document.createElement('li');
+var modalLong_Desc = document.createElement('li');
+var modalImdbScore = document.createElement('li');
+var modalBoxOffice = document.createElement('li');
+var modalActors = document.createElement('li');
+var modalDirectors = document.createElement('li');
+var modalGenres = document.createElement('li');
+var modalCountries = document.createElement('li');
+var modalRated = document.createElement('li');
 
 
 // 2.2 ajout d'attributs aux éléménts de la MODALE (1ère image Carrousel FILMS LES MIEUX NOTES)
@@ -302,7 +289,7 @@ function setThumbnailModalElementsAttributes(){
 }
 
 // 2.3 élaboration de la structure de la modale (assemblage des pièces du puzzle)
-let category1Elements = document.getElementsByClassName("category1")
+let category1Elements = document.getElementsByClassName("category1");
 let thumbnailsList = category1Elements[0].getElementsByClassName("thumbnail");
 let slidersList = category1Elements[0].getElementsByClassName("slider");
 
@@ -329,39 +316,54 @@ function buildThumbnailModal(){
     modalNoList.appendChild(modalImage);
 }
 
+
+// récupération liste des urls des films détaillés
+let detailedRatedFilmsUrlsList = [];
+
+function getDetailedRatedFilmsUrls(bestRatedFilmsList){
+    for (let film of bestRatedFilmsList){
+        detailedRatedFilmsUrlsList.push(film.url)
+    };
+    console.log(detailedRatedFilmsUrlsList);
+    return detailedRatedFilmsUrlsList; 
+}
+
+
 // 2.4 Ouverture / fermeture de la modale
-//Affiche modale qd clique sur les vignettes du carrousel "Films les mieux notés"
-for(let PicIndex in [0, 1, 2, 3]){
-    activePicSlider1[PicIndex].addEventListener('click', function(){
-        var filmsListIndex = PicIndex;
-        getBestRatedFilmDetails(bestRatedfilmsList, filmsListIndex);
+//AFFICHE MODALE qd clique sur 1ère vignette du carrousel "Films les mieux notés"
+for(let thumbnailIndex in [0, 1, 2, 3]){
+    activePicSlider1[thumbnailIndex].addEventListener('click', function(){
+        let activePicUrl = activePicSlider1[thumbnailIndex].getAttribute("src"); // OK (console.log)
+        let filmIndex = bestRatedFilmsImages.indexOf(activePicUrl); // OK (console.log)
+        let FilmUrl = detailedRatedFilmsUrlsList[filmIndex]; // OK (console.log)
+        console.log(FilmUrl);
+        getBestRatedFilmDetails(FilmUrl); // ==> PB 
         newModal.style.display = "flex";
     });
 }
 
-// Ferme la fenêtre modale de la 1ère vignette qd clique sur la croix
-modalClose.addEventListener('click', function(){
-    newModal.style.display = "none";
-});
 
 
-// 2.5 alimentation modale 1ère image Carrousel FILMS LES MIEUX NOTES 
-
-function getBestRatedFilmDetails(bestRatedfilmsList){
-    let FilmUrl = bestRatedfilmsList.results[0].url;
+//récupère LISTE des DETAILS d'UN FILM à partir de l'url du film détaillé
+function getBestRatedFilmDetails(FilmUrl){
     let httpRequest = new XMLHttpRequest();
     httpRequest.open('GET', FilmUrl, true);
     httpRequest.send();
     httpRequest.onreadystatechange = function(){
         if(httpRequest.readyState === 4){
-            if (httpRequest.status === 200) {
+            if (httpRequest.status === 200){
                 let detailedFilm = JSON.parse(httpRequest.responseText);
                 console.log(detailedFilm);
+
                 getModalFilmDetails(detailedFilm);
             }
         }
     };
 }
+
+
+// 2.5 ALIMENTATION  MODALE Carrousel FILMS LES MIEUX NOTES 
+// alimente modale avec données d'un film "detailedFilm"
 function getModalFilmDetails(detailedFilm){
     modalTitle.innerText = "Titre : "+detailedFilm.title;
     modalImage.setAttribute('src', detailedFilm.image_url);
@@ -377,8 +379,16 @@ function getModalFilmDetails(detailedFilm){
     modalRated.innerText = "rated : "+detailedFilm.rated;
 }
 
+
+// FERME la fenêtre MODALE de la vignette cliquée
+modalClose.addEventListener('click', function(){
+    newModal.style.display = "none";
+}
+);
+
+
 // 2 RECUPERE LA LISTE DES FILMS DE LA CATEGORIE (ici 'Les Mieux Notés')
-getBestRatedfilmsList() ;
+getbestRatedFilmsList() ;
 
 
 
@@ -386,8 +396,8 @@ getBestRatedfilmsList() ;
 
 
 // CARROUSELS
-//Réserve de vignettes à faire défiler(AvailableThumbnails) pour les 4 carrousels 
-/*let imagesBestRatedFilms = ["pictures/image1.jpg", "pictures/image2.jpg", 
+
+/*let bestRatedFilmsImages = ["pictures/image1.jpg", "pictures/image2.jpg", 
                         "pictures/image3.jpg", "pictures/image4.jpg", 
                         "pictures/image5.jpg", "pictures/image6.jpg", 
                         "pictures/image7.jpg"];
@@ -415,31 +425,10 @@ category2 -> Aventure
 category3 -> Animation
 category4 -> Biographie
 */
-//let activePicSlider1 =>>redéfini, avec API, plus haut
+//let activePicSlider1 = document.querySelectorAll('.category1 .slider .active'); // redéfini, avec API, plus haut
 /*let activePicSlider2 = document.querySelectorAll('.category2 .slider .active');
 let activePicSlider3 = document.querySelectorAll('.category3 .slider .active');
 let activePicSlider4 = document.querySelectorAll('.category4 .slider .active');*/
-
-//vignettes à faire défiler(AvailableThumbnails) pour les 4 carrousels
-var imagesBestRatedFilms = [];
-
-function getAvailableFilms(bestRatedFilmsImages) {
-    let httpRequest = new XMLHttpRequest();
-    httpRequest.open('GET', "http://localhost:8000/api/v1/titles/?format=json&page=2&sort_by=rated,-year", true);
-    httpRequest.send();
-    httpRequest.onreadystatechange = function(){
-        if(httpRequest.readyState === 4){
-            if (httpRequest.status === 200) {
-                let SecondPageFilmsList = JSON.parse(httpRequest.responseText);
-                for(let ratedFilm = 0; ratedFilm < 2; ratedFilm++){
-                    imagesBestRatedFilms = bestRatedFilmsImages.push(SecondPageFilmsList.results[ratedFilm].image_url);
-                }
-                console.log(imagesBestRatedFilms);
-                return imagesBestRatedFilms;
-            }
-        }
-    } 
-}
 
 
 
@@ -460,6 +449,7 @@ let count1 = 0;
 let count2 = 0;
 let count3 = 0;
 let count4 = 0;
+
 
 class Slider{
     constructor(activeThumbnails, availableThumbnails, nextThumbnail, previousThumbnail, count){
@@ -495,49 +485,51 @@ class Slider{
         }
     }
 }
-
-// création 1er slider (1ère instance de la classe Slider)
-let bestFilmsSlider = new Slider(activePicSlider1, 
-                        imagesBestRatedFilms, 
-                        nextThumbnail1,
-                        previousThumbnail1,
-                        count1);
+//bestRatedFilmsImages
+// création 1er slider (1ère instance de la classe Slider) 'availableThumbnails' = 'bestRatedFilmsImages'
+let bestRatedFilmsSlider = new Slider(activePicSlider1, 
+    bestRatedFilmsImages,
+    nextThumbnail1,
+    previousThumbnail1,
+    count1);
 
 /* création 2e slider (2e instance de la classe Slider)
 let AdventureSlider = new Slider(activePicSlider2, 
-                        imagesAdventure, 
-                        nextThumbnail2,
-                        previousThumbnail2,
-                        count2);
+    imagesAdventure, 
+    nextThumbnail2,
+    previousThumbnail2,
+    count2);
 
 // création 3e slider (3e instance de la classe Slider)
 let AnimationSlider = new Slider(activePicSlider3, 
-                        imagesAnimation, 
-                        nextThumbnail3,
-                        previousThumbnail3,
-                        count3);
+    imagesAnimation, 
+    nextThumbnail3,
+    previousThumbnail3,
+    count3);
 
 // création 4e slider (4e instance de la classe Slider)
 let BiographySlider = new Slider(activePicSlider4, 
-                        imagesBiography, 
-                        nextThumbnail4,
-                        previousThumbnail4,
-                        count4);
+    imagesBiography, 
+    nextThumbnail4,
+    previousThumbnail4,
+    count4);
 */
 
 // détection de clic sur boutons précédent/suivant 1 et lancement réaction
-bestFilmsSlider.previousThumbnail.addEventListener('click', function(){
-    bestFilmsSlider.count--;
-    bestFilmsSlider.UpdatingCount(this.count);
-    bestFilmsSlider.changeThumbnail(this.activeThumbnails, this.availableThumbnails, this.count);
+bestRatedFilmsSlider.previousThumbnail.addEventListener('click', function(){
+    bestRatedFilmsSlider.count--;
+    bestRatedFilmsSlider.UpdatingCount(this.count);
+    bestRatedFilmsSlider.changeThumbnail(this.activeThumbnails, this.availableThumbnails, this.count);
 });
 
-bestFilmsSlider.nextThumbnail.addEventListener('click', function(){
-    bestFilmsSlider.count++;
-    bestFilmsSlider.UpdatingCount(this.count);
-    bestFilmsSlider.changeThumbnail(this.activeThumbnails, this.availableThumbnails, this.count);
+bestRatedFilmsSlider.nextThumbnail.addEventListener('click', function(){
+    bestRatedFilmsSlider.count++;
+    bestRatedFilmsSlider.UpdatingCount(this.count);
+    bestRatedFilmsSlider.changeThumbnail(this.activeThumbnails, this.availableThumbnails, this.count);
 });
-/*
+
+
+/* 
 // détection de clic sur boutons précédent/suivant 2 et lancement réaction
 AdventureSlider.previousThumbnail.addEventListener('click', function(){
     AdventureSlider.count--;
